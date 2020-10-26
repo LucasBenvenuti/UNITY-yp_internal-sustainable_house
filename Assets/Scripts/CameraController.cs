e6f81d6bed0ae89bbd51ead1eee438098b807af5
Gabriela Bobra <gabibobra@gmail.com>
2020-10-26 11:57:09 -0300

    Lean touch tests

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Lean.Touch;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,10 @@ public class CameraController : MonoBehaviour
     public Vector3 itemZoomOffset;
     public Vector3 cameraBasePosition;
     public bool zoomControl;
+    public bool zoomTeste;
+    public LeanDragCamera leanDrag;
+    public LeanTwistRotateAxis leanTwist;
+    public LeanPinchScale leanPinch;
 
     private void Awake()
     {
@@ -35,18 +40,27 @@ public class CameraController : MonoBehaviour
         {
             LerpToZoomPosition(GameController.instance.itemHolder);
         }
-        else
+        else if (!zoomControl && zoomTeste)
         {
             ReturnToBasePosition();
         }
     }
     public void LerpToZoomPosition(GameObject itemSelected)
     {
+        leanDrag.enabled = false;
+        leanTwist.enabled = false;
+        leanPinch.enabled = false;
         Vector3 itemZoomPosition = itemSelected.transform.position + itemZoomOffset;
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, itemZoomPosition, Time.deltaTime * 5);
+        zoomTeste = true;
+
+
     }
     public void ReturnToBasePosition()
     {
+        leanDrag.enabled = true;
+        leanTwist.enabled = true;
+        leanPinch.enabled = true;
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, cameraBasePosition, Time.deltaTime * 5);
         GameController.instance.panelItem.SetActive(false);
     }
---
+++ Assets/Scripts/ClampTransform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampTransform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -36.5f, 37.5f), Mathf.Clamp(transform.position.y, 10, 60), Mathf.Clamp(transform.position.z, -45.5f, 20.5f));
+    }
+}
