c38245ff54d9cc397906e56f557afe84b7bb5db2
Lucas Benvenuti <lucasbenvenuti2010@gmail.com>
2020-11-04 18:01:52 -0300

    Register now saves Player's First and Last names

--- Assets/Scripts/Register/RegisterController.cs
+++ Assets/Scripts/Register/RegisterController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using TMPro;
 
 public class RegisterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_InputField firstNameInput;
+    public TMP_InputField lastNameInput;
+
     void Start()
     {
-        
+        PlayerPrefs.DeleteKey("Player_FirstName");
+        PlayerPrefs.DeleteKey("Player_LastName");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegisterUser()
     {
-        
+        //NEED TO CREATE VERIFICATION TO INPUTS THAT WERE NOT USED
+
+        string fullName = firstNameInput.text + " " + lastNameInput.text;
+
+        PlayerPrefs.SetString("Player_Name", fullName);
+
+        Debug.Log("Saved First Name - " + PlayerPrefs.GetString("Player_Name"));
     }
 }
