12bae47a572a3406e4b17c00794b3fe94b91681f
Lucas Benvenuti <lucasbenvenuti2010@gmail.com>
2020-11-16 14:50:44 -0300

    Added new function to put on TVs and new material to be applied on TV Screens

---
+++ Assets/Scripts/TV_Screen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV_Screen : MonoBehaviour
+{
+    public Material screenMat;
+    public Texture2D[] imagesList;
+    public float imageDuration = 4f;
+    int currentIndex;
+
+    // int teste1 = -8;
+    // int teste2 = 1;
+    // int teste3 = 2;
+    // int teste4 = 3;
+
+    // valorTotal = teste1 + teste2 + teste3 + teste4
+
+    void Start()
+    {
+        currentIndex = 0;
+        screenMat.mainTexture = imagesList[currentIndex];
+
+        StartCoroutine(changeMatSprite());
+    }
+
+    IEnumerator changeMatSprite()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(imageDuration);
+
+            if (currentIndex >= (imagesList.Length - 1))
+            {
+                currentIndex = 0;
+            }
+            else
+            {
+                currentIndex++;
+            }
+
+            screenMat.mainTexture = imagesList[currentIndex];
+        }
+    }
+}
