4a4f4f20ff2322ccbdd9e2899450e6693563123b
Gabriela Bobra <gabibobra@gmail.com>
2020-11-20 17:48:13 -0300

    background audio

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float animDuration;
+    public LeanTweenType easeInOut;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void ToggleBackgroundAudio()
+    {
+        if (audioSource.volume != 0f)
+        {
+            LeanTween.value(this.gameObject, this.audioSource.volume, 0f, animDuration).setEase(easeInOut).setOnUpdate((float flt) =>
+            {
+                audioSource.volume = flt;
+            });
+        }
+        else
+        {
+            LeanTween.value(this.gameObject, this.audioSource.volume, 0.15f, animDuration).setEase(easeInOut).setOnUpdate((float flt) =>
+ {
+     audioSource.volume = flt;
+ });
+        }
+    }
+}
