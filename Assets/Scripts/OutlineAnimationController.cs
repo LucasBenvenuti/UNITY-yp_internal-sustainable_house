c1a5ab2b7a7279cd4be164b39e48a55790e637f4
Lucas Benvenuti <lucasbenvenuti2010@gmail.com>
2020-11-25 15:52:32 -0300

    Added new outline layer to each layer obey his own outline area

---
+++ Assets/Scripts/OutlineAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EPOOutline;
+
+public class OutlineAnimationController : MonoBehaviour
+{
+    public LeanTweenType easeInOut;
+    public float tweenDuration = 1f;
+
+    public Outliner[] outlineArray;
+    public GameObject mainCamera;
+
+    void Start()
+    {
+        outlineArray = mainCamera.GetComponents<Outliner>();
+
+        foreach (Outliner lines in outlineArray)
+        {
+            LeanTween.value(this.gameObject, 0.5f, 2, tweenDuration).setEase(easeInOut).setOnUpdate((float flt) =>
+                {
+                    lines.DilateShift = flt;
+                }).setLoopPingPong();
+        }
+    }
+}
