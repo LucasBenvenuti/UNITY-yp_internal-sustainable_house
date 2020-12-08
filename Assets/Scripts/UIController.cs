539c4eb5a149f543000ac1770bf7d342ffa44a9e
Gabriela Bobra <gabibobra@gmail.com>
2020-12-08 16:20:12 -0300

    new animation for increase and decrease values of money and sustainability

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
@@ -23,6 +24,13 @@ public class UIController : MonoBehaviour
     float controlMoney;
     float controlSustainability;
 
+    public Animator moneyHelperAnimator;
+    public TMP_Text moneyHelperText;
+    public GameObject[] moneyHelperIcon;
+
+    public Animator sustainabilityHelperAnimator;
+    public TMP_Text sustainabilityHelperText;
+    public GameObject[] sustainabilityHelperIcon;
 
     public GameObject[] itemsSelectables;
 
@@ -80,6 +88,39 @@ public class UIController : MonoBehaviour
         float neutralSus = controlSustainability - oldSus;
         float newMoneyValue = neutralMoney + newPrice;
         float newSusValue = neutralSus + newSus;
+
+        if(newPrice >= 0)
+        {
+            moneyHelperText.text = "+" + newPrice.ToString();
+            moneyHelperText.color = new Color32(44, 201, 130, 255);
+            moneyHelperIcon[0].SetActive(true);
+            moneyHelperIcon[1].SetActive(false);
+            moneyHelperAnimator.SetTrigger("IncreaseMoney");
+        }
+        else
+        {
+             moneyHelperText.text = newPrice.ToString();
+            moneyHelperText.color = new Color32(227, 36, 98, 255);
+            moneyHelperIcon[1].SetActive(true);
+            moneyHelperIcon[0].SetActive(false);
+            moneyHelperAnimator.SetTrigger("DecreaseMoney");
+        }
+        if(newSus >= 0)
+        {
+            sustainabilityHelperText.text = "+" + newSus.ToString();
+            sustainabilityHelperText.color =  new Color32(178, 108, 215, 255);
+            sustainabilityHelperIcon[0].SetActive(true);
+            sustainabilityHelperIcon[1].SetActive(false);
+            sustainabilityHelperAnimator.SetTrigger("IncreaseSustainability");
+        }
+        else
+        {
+            sustainabilityHelperText.text = newSus.ToString();
+            sustainabilityHelperText.color = new Color32(227, 36, 98, 255);
+            sustainabilityHelperIcon[1].SetActive(true);
+            sustainabilityHelperIcon[0].SetActive(false);
+            sustainabilityHelperAnimator.SetTrigger("DecreaseSustainability");
+        }
 
         DataStorage.instance.addReportLine("Recursos anteriores: " + controlMoney + ". Novos Recursos: " + newMoneyValue + ".");
         DataStorage.instance.addReportLine("Sustentabilidade anterior: " + controlSustainability + ". Nova Sustentabilidade: " + newSusValue + ".");
