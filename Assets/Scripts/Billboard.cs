9940fc1a675299674f5c2ae9d6a1784bbedd4e53
Gabriela Bobra <gabibobra@gmail.com>
2020-12-17 09:30:00 -0300

    bigger actions sprite and fade test

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -33,13 +33,12 @@ public class Billboard : MonoBehaviour
 
     IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time, float alpha)
     {
-        LeanTween.alpha(this.gameObject, alpha, 1.34f);
+        LeanTween.alpha(this.gameObject, alpha, 1f);
         float i = 0.0f;
         float rate = (1.0f / time) * speed;
         while (i < 1.0f)
         {
             i += Time.deltaTime * rate;
-
             transform.localScale = Vector3.Lerp(a, b, i);
             yield return null;
         }
